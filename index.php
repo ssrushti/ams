4a0b4bf14acefc356587d5a7ad2b7ceba3fdbaa1
ssrushti <45996161+ssrushti@users.noreply.github.com>
2020-05-03 15:06:47 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 <frameset rows="10%, 8%, *" border="o">
 <frame src="title.html" noresize="noresize" name="title" scrolling="no">
-<frame src="adcontents.html" noresize="noresize"name="contents" scrolling="no">
-<frame src="adminprofile.php" noresize="noresize"  name="home">
+<frame src="indexcontents.html" noresize="noresize"name="contents" scrolling="no">
+<frame src="login.php" noresize="noresize" name="home">
 </frameset>
 </head>
 </html>
